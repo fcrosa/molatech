dea6b00b7f4a3658ab2e62ee46da0e708a8dda7e
Fernando Crosa <crosafernando@gmail.com>
2025-06-12 12:46:34 +0200

    remove unused methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,15 +7,15 @@ class Order < ApplicationRecord
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
   validates :created_at, presence: true
 
-  def commission_rate
-    case amount
-    when 0...50 then 0.01
-    when 50...300 then 0.0095
-    else 0.0085
-    end
-  end
+#  def commission_rate
+#    case amount
+#    when 0...50 then 0.01
+#    when 50...300 then 0.0095
+#    else 0.0085
+#    end
+#  end
 
-  def commission_amount
-    (amount * commission_rate).round(2)
-  end
+#  def commission_amount
+#    (amount * commission_rate).round(2)
+#  end
 end
